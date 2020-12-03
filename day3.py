0695c476fc40d72ef5f5465d9e5b23f688a4c675
Hofei90 <29521028+Hofei90@users.noreply.github.com>
2020-12-03 07:01:57 +0100

    Solution Day 3 #2

--- day3.py
+++ day3.py
@@ -1,4 +1,5 @@
 import os
+from day1 import produkt_of_right_combinations
 
 SKRIPTPFAD = os.path.abspath(os.path.dirname(__file__))
 
@@ -16,10 +17,13 @@ class Navigator:
         self.y += self.down
 
     def check_crash(self):
-        x_pointer = self.x % len(self.matrix[self.y])
-        if self.matrix[self.y][x_pointer]:
-            return True
-        else:
+        try:
+            x_pointer = self.x % len(self.matrix[self.y])
+            if self.matrix[self.y][x_pointer]:
+                return True
+            else:
+                return False
+        except IndexError:
             return False
 
 
@@ -47,6 +51,8 @@ def read_input(datei):
 
 def main():
     matrix = generate_matrix(read_input(os.path.join(SKRIPTPFAD, "input_3_1")))
+
+    # Day 3 Nr. 1
     navigator = Navigator(1, 3, matrix)
     counter = 0
     for _ in matrix:
@@ -55,6 +61,19 @@ def main():
         navigator.drive()
     print(f"Solution auf Day 3 #1: {counter}")
 
+    # Day 3 Nr. 2
+    solutions = []
+    coordinates = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    for x, y in coordinates:
+        navigator = Navigator(y, x, matrix)
+        counter = 0
+        for _ in matrix:
+            if navigator.check_crash():
+                counter += 1
+            navigator.drive()
+        solutions.append(counter)
+
+    print(f"Solution auf Day 3 #2: {produkt_of_right_combinations(solutions)}")
 
 if __name__ == "__main__":
     main()
