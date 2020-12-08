be7088ce2f7a0ad9410c3ed0e85dbe2db4894b31
Hofei90 <29521028+Hofei90@users.noreply.github.com>
2020-12-08 18:34:22 +0100

    tag 8 teil 2

--- day8.py
+++ day8.py
@@ -1,4 +1,5 @@
 import os
+import copy
 
 SKRIPTPFAD = os.path.abspath(os.path.dirname(__file__))
 
@@ -11,7 +12,10 @@ class InfinityGuard:
         self.position[index] = True
 
     def get_position(self, index):
-        return self.position[index]
+        try:
+            return self.position[index]
+        except IndexError:
+            return True
 
 
 class Bootloader:
@@ -20,9 +24,12 @@ class Bootloader:
         self.index = 0
         self.accumulator = 0
         self.infinity_guard = infinity_guard
+        self.ende = False
 
     def excecute_befehl(self):
         index = self.index
+        if index == len(self.befehle):
+            self.ende = True
         if not self.infinity_guard.get_position(index):
             if self.befehle[index][0] == "acc":
                 self.accumulator += int(self.befehle[index][1])
@@ -49,6 +56,20 @@ def read_input(datei):
     return [line.strip().split() for line in inhalt]
 
 
+def repair_bootloader(original_anweisungen):
+    anweisungsvarationen = []
+    anweisungen = copy.deepcopy(original_anweisungen)
+    for index, anweisung in enumerate(anweisungen):
+        tmp_anweisungen = copy.deepcopy(original_anweisungen)
+        if anweisung[0] == "nop":
+            tmp_anweisungen[index][0] = "jmp"
+            anweisungsvarationen.append(tmp_anweisungen)
+        elif anweisung[0] == "jmp":
+            tmp_anweisungen[index][0] = "nop"
+            anweisungsvarationen.append(tmp_anweisungen)
+    return anweisungsvarationen
+
+
 def main():
     anweisungen = read_input(os.path.join(SKRIPTPFAD, "input_8_1"))
     infinity_guard = InfinityGuard(anweisungen)
@@ -57,6 +78,17 @@ def main():
     while running_bootloader:
         running_bootloader = bootloader.excecute_befehl()
     print(f"Lösung Teil 1, Accumulator: {bootloader.accumulator}")
+
+    anweisungsvariationen = repair_bootloader(anweisungen)
+    for anweisungen in anweisungsvariationen:
+        infinity_guard = InfinityGuard(anweisungen)
+        bootloader = Bootloader(anweisungen, infinity_guard)
+        running_bootloader = True
+        while running_bootloader:
+            running_bootloader = bootloader.excecute_befehl()
+            if bootloader.ende:
+                print(f"Lösung Teil 2, Accumulator: {bootloader.accumulator}")
+                break
 
 
 if __name__ == "__main__":
