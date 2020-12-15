130bb8bd6725f6e4a3eddc0cb1d2466ea490175e
Hofei90 <29521028+Hofei90@users.noreply.github.com>
2020-12-15 07:17:32 +0100

    Lösung Tag 15 Teil 2

--- day15.py
+++ day15.py
@@ -1,7 +1,7 @@
 
 STARTNUMBERS = [8, 13, 1, 0, 18, 9]
-TURNS = 2020
-
+TURNS_1 = 2020
+TURNS_2 = 30000000
 
 def speak_number(speeched_numbers, number, turn):
     try:
@@ -21,18 +21,23 @@ def calc_next_number(last_rounds):
         return last_rounds[-1] - last_rounds[-2]
 
 
-def main():
+def play_game(startnumbers, max_turns):
     speeched_numbers = {}
-    number = STARTNUMBERS[0]
-    for turn in range(1, TURNS + 1):
-        if turn <= len(STARTNUMBERS):
-            number = STARTNUMBERS[turn - 1]
+    number = startnumbers[0]
+    for turn in range(1, max_turns + 1):
+        if turn <= len(startnumbers):
+            number = startnumbers[turn - 1]
             speak_number(speeched_numbers, number, turn)
         else:
             last_rounds = get_last_turns(speeched_numbers, number)
             number = calc_next_number(last_rounds)
             speak_number(speeched_numbers, number, turn)
-    print(f"The 2020th number spoken is: {number}")
+    return number
+
+
+def main():
+    print(f"The {TURNS_1}th number spoken is: {play_game(STARTNUMBERS, TURNS_1)}")
+    print(f"The {TURNS_2}th number spoken is: {play_game(STARTNUMBERS, TURNS_2)}")
 
 
 if __name__ == "__main__":
