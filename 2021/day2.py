52acd2b2fd0d2f93d5847d012f1700affd7c1e22
Hofei90 <29521028+Hofei90@users.noreply.github.com>
2021-12-02 07:09:55 +0100

    2_2

--- 2021/day2.py
+++ 2021/day2.py
@@ -16,8 +16,6 @@ class Navigator:
         value = int(cmd[1])
         if direction == "forward":
             self.move_forward(value)
-        elif direction == "backward":
-            self.move_backward(value)
         elif direction == "down":
             self.move_down(value)
         elif direction == "up":
@@ -26,14 +24,39 @@ class Navigator:
     def move_forward(self, value):
         self.horizontal += value
 
-    def move_backward(self, value):
-        self.horizontal -= value
-
     def move_down(self, value):
         self.tiefe += value
 
     def move_up(self, value):
         self.tiefe -= value
+
+
+class Navigator2:
+    def __init__(self, horizontal, tiefe, ziel):
+        self.horizontal = horizontal
+        self.tiefe = tiefe
+        self.ziel = ziel
+
+    def move(self, befehl):
+        cmd = befehl.split()
+        direction = cmd[0]
+        value = int(cmd[1])
+        if direction == "forward":
+            self.move_forward(value)
+        elif direction == "down":
+            self.move_down(value)
+        elif direction == "up":
+            self.move_up(value)
+
+    def move_forward(self, value):
+        self.horizontal += value
+        self.tiefe += value * self.ziel
+
+    def move_down(self, value):
+        self.ziel += value
+
+    def move_up(self, value):
+        self.ziel -= value
 
 
 def calc_erg1(x, y):
@@ -43,9 +66,12 @@ def calc_erg1(x, y):
 def main():
     planned_course = INPUT1.read_text().split("\n")
     navigator = Navigator(0, 0)
+    navigator2 = Navigator2(0, 0, 0)
     for cmd in planned_course:
         navigator.move(cmd)
+        navigator2.move(cmd)
     print(f"Ergebnis: {calc_erg1(navigator.tiefe, navigator.horizontal)}")
+    print(f"Ergebnis: {calc_erg1(navigator2.tiefe, navigator2.horizontal)}")
 
 
 if __name__ == "__main__":
