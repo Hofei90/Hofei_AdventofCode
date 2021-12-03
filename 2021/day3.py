7b58b1e15832ff58b087a214088dd90e0d304b26
Hofei90 <29521028+Hofei90@users.noreply.github.com>
2021-12-03 14:31:51 +0100

    3_2

--- 2021/day3.py
+++ 2021/day3.py
@@ -9,7 +9,6 @@ def generate_int(rate):
     return int(str_, 2)
 
 
-
 def calc_power_consumption(gamma_rate, epsilon_rate):
     gamma_rate = generate_int(gamma_rate)
     epsilon_rate = generate_int(epsilon_rate)
@@ -39,10 +38,63 @@ def analyze_most_common_bit(diagnostic_report):
     return generate_rates(sum_numbers, high_counter)
 
 
+# ---- Teil 2 ---- '
+def analyze_most_common_bit_oxygen(diagnostic_report, index):
+    sum_numbers = len(diagnostic_report)
+    high = 0
+    for number in diagnostic_report:
+        if int(number[index]):
+            high += 1
+    if high >= (sum_numbers / 2):
+        keep = "1"
+    else:
+        keep = "0"
+    new_report = []
+    for number in diagnostic_report:
+        if number[index] == keep:
+            new_report.append(number)
+    return new_report
+
+
+def analyze_most_common_bit_cozwei(diagnostic_report, index):
+    sum_numbers = len(diagnostic_report)
+    high = 0
+    for number in diagnostic_report:
+        if int(number[index]):
+            high += 1
+    if high < (sum_numbers / 2):
+        keep = "1"
+    else:
+        keep = "0"
+    new_report = []
+    for number in diagnostic_report:
+        if number[index] == keep:
+            new_report.append(number)
+    return new_report
+
+
 def main():
     diagnostic_report = INPUT1.read_text().splitlines()
     gamma_rate, epsilon_rate = analyze_most_common_bit(diagnostic_report)
     print(f"Power Consumption: {calc_power_consumption(gamma_rate, epsilon_rate)}")
+
+    # Teil 2
+    report = diagnostic_report
+
+    index = 0
+    while len(report) > 1:
+        report = analyze_most_common_bit_oxygen(report, index)
+        index += 1
+    oxy = int(report[0], 2)
+
+    report = diagnostic_report
+
+    index = 0
+    while len(report) > 1:
+        report = analyze_most_common_bit_cozwei(report, index)
+        index += 1
+    co2 = int(report[0], 2)
+    print(co2 * oxy)
 
 
 if __name__ == "__main__":
